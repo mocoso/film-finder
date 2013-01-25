24249e0df2eaafdc9bbf473cfd611f2ad1abc3a5
Joel Chippindale <joel@joelchippindale.com>
2013-01-25 22:12:59 +0000

    Initialize rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FilmFinder::Application.config.secret_token = '9327562c4207a2b013870df16ba039c41b2fe2fd96e2da9552527c5e421e4e17d6ea32a1286ed2374af2062024863ad006a4bdb0b699593cf0b6ae641a217c8b'
