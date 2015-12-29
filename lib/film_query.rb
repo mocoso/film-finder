d43cbcf139a0c6548d66225248f3788bdbdea1d8
Joel Chippindale <joel@joelchippindale.com>
2015-12-29 21:25:16 +0000

    Order so that films with most availability come top

--- lib/film_query.rb
+++ lib/film_query.rb
@@ -19,7 +19,7 @@ class FilmQuery
       group_by { |sr| sr.title.downcase }.
       map { |f| Film.new(f.last.first.title, :rentals => f.last) }.
       select { |f| f.number_of_words_matching_query(query) > 0 }. # Reject those who don't seem good enough matches - I am looking at you Google Play
-      sort_by { |f| [f.number_of_words_matching_query(query), f.rentals.size] }.reverse # Prioritize those which are returned by multiple services
+      sort_by { |f| [f.rentals.size, f.number_of_words_matching_query(query)] }.reverse # Prioritize those which are returned by multiple services
   end
 
   def unavailable_sources
