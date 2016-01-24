285bd7ac48ab7fed6c36d3648ccc2250875fcf05
Joel Chippindale <joel@joelchippindale.com>
2016-01-24 20:47:22 +0000

    Switch to new blinkbox gem

--- lib/source/blinkbox.rb
+++ lib/source/blinkbox.rb
@@ -16,13 +16,13 @@ module Source
       BlinkboxFilms::Search.new.search(query).take(MAX_NUMBER_OF_RESULTS).each_with_index.map { |result, i|
         Rental.new \
           :service => name,
-          :title => Title.new(result[:title]),
-          :url => result[:url],
-          :image_url => result[:image_url],
+          :title => Title.new(result.title),
+          :url => result.url,
+          :image_url => result.image_url,
           :image_quality_rank => 3,
-          :certificate => result[:certificate],
+          :certificate => result.certificate,
           :type => Rental::FILM_TYPE,
-          :length_in_minutes => result[:running_time_in_minutes],
+          :length_in_minutes => result.running_time_in_minutes,
           :search_rank => i
       }
     end
