dbf8a49624ab4c5883c328d6ee5d5f79c18d0cca
Joel Chippindale <joel@joelchippindale.com>
2016-10-22 08:50:17 +0100

    Require action_view railtie

--- config/application.rb
+++ config/application.rb
@@ -4,6 +4,7 @@ require_relative 'boot'
 # require "active_record/railtie"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
 
